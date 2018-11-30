95f33d3f3e622f3d317e43c811b2f2e080334333
valentina <valentinaescobarm@gmail.com>
2018-11-30 18:08:08 -0300

    home listo y detalles banner slider y append

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,12 @@
+$(document).ready(function(){
 
-$(document).ready(function() {
+    $('#saludo').on('click', function(){
+        $('#saludo').append('<p id="bienvenido">BIENVENIDO <i class="fab fa-angellist"></i></p>');
 
-    var allPict=$('.gallery img');
-    var  nextImg=$('#nextImg');
-    var prevImg=$('#prevImg');
-    var currentImg=0;
-    var t=700;
-
-    allPict.eq(currentImg).show();
-
-    nextImg.on('click',function(){
-        allPict.eq(currentImg).fadeOut(t,function(){
-            allPict.hide();
-            allPict.eq(currentImg).fadeIn(t);
-        });
-        currentImg++;
-        if(currentImg>=allPict.length){
-            currentImg=0;
-        }
-
+        
     });
 
 
 
-    prevImg.on('click',function(){
-        allPict.eq(currentImg).fadeOut(t,function(){
-            allPict.hide();
-            allPict.eq(currentImg).fadeIn(t);
-        });
-        currentImg--;
-        if (currentImg<0){
-            currentImg=allPict.length-1;
-        }
 
-    });
-
-});
-    //$(document).on("click", "#slide", function(){
-    //    var source = $(this).attr('src'),
-    //    console.log()
+})
