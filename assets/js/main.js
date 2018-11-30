25eba7d3a5ddb325fbc0238730e2b0143c27dd88
valentina <valentinaescobarm@gmail.com>
2018-11-30 19:12:47 -0300

    galeria jquery en pagina cursos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
 
+    // append index
     $('#saludo').on('click', function(){
         $('#saludo').append('<p id="bienvenido">BIENVENIDO <i class="fab fa-angellist"></i></p>');
 
-        
+
     });
+
+    // galeria cursos html
+    $('.muestra').on('click', function(){
+        source = $(this).attr('src');
+        $('.principal').attr('src', source);
+
+
+    });
+
+
 
 
 
