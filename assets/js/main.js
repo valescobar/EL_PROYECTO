d6a1d609daa628e9885485962229e54085ee2a65
valentina <valentinaescobarm@gmail.com>
2018-11-30 16:49:18 -0300

    barra de navegacion con link a 5 vistas y algo más

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+$(document).ready(function() {
+
+    var allPict=$('.gallery img');
+    var  nextImg=$('#nextImg');
+    var prevImg=$('#prevImg');
+    var currentImg=0;
+    var t=700;
+
+    allPict.eq(currentImg).show();
+
+    nextImg.on('click',function(){
+        allPict.eq(currentImg).fadeOut(t,function(){
+            allPict.hide();
+            allPict.eq(currentImg).fadeIn(t);
+        });
+        currentImg++;
+        if(currentImg>=allPict.length){
+            currentImg=0;
+        }
+
+    });
+
+
+
+    prevImg.on('click',function(){
+        allPict.eq(currentImg).fadeOut(t,function(){
+            allPict.hide();
+            allPict.eq(currentImg).fadeIn(t);
+        });
+        currentImg--;
+        if (currentImg<0){
+            currentImg=allPict.length-1;
+        }
+
+    });
+
+});
+    //$(document).on("click", "#slide", function(){
+    //    var source = $(this).attr('src'),
+    //    console.log()
--- script.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-    $('#container').masonry({
-      // options
-      itemSelector : '.item',
-    });
-});
